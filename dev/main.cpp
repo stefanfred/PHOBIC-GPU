a885a5f1fb255700eec850217766441617187b1a
ByteHamster <info@bytehamster.com>
2024-02-14 15:42:14 +0100

    Add command line parser

--- dev/main.cpp
+++ dev/main.cpp
@@ -1,18 +1,19 @@
-#include "mphf_builder.h"
-#include "encoders/pilotEncoders/mono_encoders.hpp"
-#include "encoders/pilotEncoders/ortho_encoder_dual.hpp"
-#include "encoders/partitionOffsetEnocders/direct_partition_offset_encoder.hpp"
-
-
-using namespace pthash;
-
-
-#define DO_NOT_OPTIMIZE(value) asm volatile("" : : "r,m"(value) : "memory")
-
+#include <chrono>
+#include <mphf_builder.h>
+#include <encoders/pilotEncoders/mono_encoders.hpp>
+#include <encoders/partitionOffsetEnocders/direct_partition_offset_encoder.hpp>
+#include <tlx/cmdline_parser.hpp>
 
 int main(int argc, char *argv[]) {
     std::cout.precision(3);
     std::cout << std::fixed;
+    size_t size = 1e4;
+
+    tlx::CmdlineParser cmd;
+    cmd.add_bytes('n', "size", size, "Number of objects to construct with");
+    if (!cmd.process(argc, argv)) {
+        return 1;
+    }
 
     AppConfiguration config;
     config.appName = "MPHF";
@@ -20,24 +21,22 @@ int main(int argc, char *argv[]) {
     App app(config);
     app.debugInfo();
 
-    size_t size = 1e4;
-
-    MPHFbuilder builder(app, MPHFconfig(7.f, 1024));
-
-    MPHF<mono_encoder<compact>, direct_partition_encoder<compact>> f;
-
     std::vector<Key> keys;
     keys.reserve(size);
-
     std::random_device rd;
     std::mt19937_64 gen(rd());
     std::uniform_int_distribution<uint32_t> dis;
-
     for (size_t i = 0; i < size; ++i) {
         keys.push_back({dis(gen), dis(gen), dis(gen), dis(gen)});
     }
 
+    MPHFbuilder builder(app, MPHFconfig(7.f, 1024));
+    MPHF<mono_encoder<compact>, direct_partition_encoder<compact>> f;
+    auto beginConstruction = std::chrono::high_resolution_clock::now();
     builder.build(keys, f);
+    unsigned long constructionDurationMs = std::chrono::duration_cast<std::chrono::milliseconds>(
+                   std::chrono::high_resolution_clock::now() - beginConstruction).count();
+    std::cout << "Construction took " << constructionDurationMs << " ms" << std::endl;
 
     std::vector<bool> taken(keys.size(), false);
     for (size_t i = 0; i < keys.size(); i++) {
